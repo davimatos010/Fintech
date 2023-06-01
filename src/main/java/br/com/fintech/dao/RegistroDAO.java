a4c4aefe7e5b49e660716ea40674646206660842
Victor Bussiki <victorbussiki@gmail.com>
2023-06-01 09:36:42 -0300

    (refactor) melhorias contra injeção sql no DAO registros

--- src/main/java/br/com/fintech/dao/RegistroDAO.java
+++ src/main/java/br/com/fintech/dao/RegistroDAO.java
@@ -16,7 +16,26 @@ import br.com.fintech.utils.DateParser;
 
 public class RegistroDAO {
 	private Connection connection;
-
+	
+	private Registro handleQuery(ResultSet res) throws SQLException {
+		Usuario usuario = new Usuario();
+		usuario.setCodigo(res.getInt("cd_usuario"));
+		
+		Tipo tipo = new Tipo();
+		tipo.setCodigo(res.getInt("cd_tipo"));
+		
+		Registro registro = new Registro();
+		registro.setCodigo(res.getInt("cd_registro"));
+		registro.setUsuario(usuario);
+		registro.setValor(res.getDouble("vl_registro"));
+		registro.setDataRegistro(DateParser.SQLToLocalDateTime(res.getDate("dt_registro")));
+		registro.setTipo(tipo);
+		registro.setCategoria(res.getString("ds_categoria"));
+		registro.setDescricao(res.getString("ds_registro"));
+		
+		return registro;
+	}
+	
 	public void insert(Registro registro) {
 		PreparedStatement stmt = null;
 		Date dataRegistro = DateParser.localDateTimeToSQL(registro.getDataRegistro());
@@ -66,25 +85,15 @@ public class RegistroDAO {
 		try {
 			connection = OracleDBConnection.getInstance().getConnection();
 			stmt = connection.prepareStatement("SELECT * FROM T_FINTECH_REGISTRO "
-					+ "WHERE cd_usuario = " + codigoUsuario + " ORDER BY dt_registro");
+					+ "WHERE cd_usuario = ? ORDER BY dt_registro DESC");
+			
+			stmt.setInt(1, codigoUsuario);
+			
 			res = stmt.executeQuery();
 
 			while(res.next()) {
 				
-				Usuario usuario = new Usuario();
-				usuario.setCodigo(res.getInt("cd_usuario"));
-				
-				Tipo tipo = new Tipo();
-				tipo.setCodigo(res.getInt("cd_tipo"));
-				
-				Registro registro = new Registro();
-				registro.setCodigo(res.getInt("cd_registro"));
-				registro.setUsuario(usuario);
-				registro.setValor(res.getDouble("vl_registro"));
-				registro.setDataRegistro(DateParser.SQLToLocalDateTime(res.getDate("dt_registro")));
-				registro.setTipo(tipo);
-				registro.setCategoria(res.getString("ds_categoria"));
-				registro.setDescricao(res.getString("ds_registro"));
+				Registro registro = this.handleQuery(res);
 
 				list.add(registro);
 			}
@@ -119,27 +128,16 @@ public class RegistroDAO {
 			stmt = connection.prepareStatement("SELECT cd_registro, cd_usuario, vl_registro, "
 					+ "dt_registro, cd_tipo, ds_categoria, ds_registro "
 					+ "FROM T_FINTECH_REGISTRO "
-					+ "WHERE cd_usuario = " + codigoUsuario  
-					+ " AND cd_registro = " +  codigoRegistro);
+					+ "WHERE cd_usuario = ?"  
+					+ " AND cd_registro = ?");
+			
+			stmt.setInt(1, codigoUsuario);
+			stmt.setInt(2, codigoRegistro);
+			
 			res = stmt.executeQuery();
 
 			if(res.next()) {
-				
-				Usuario usuario = new Usuario();
-				usuario.setCodigo(res.getInt("cd_usuario"));
-				
-				Tipo tipo = new Tipo();
-				tipo.setCodigo(res.getInt("cd_tipo"));
-				
-				registro = new Registro();
-				registro.setCodigo(res.getInt("cd_registro"));
-				registro.setUsuario(usuario);
-				registro.setValor(res.getDouble("vl_registro"));
-				registro.setDataRegistro(DateParser.SQLToLocalDateTime(res.getDate("dt_registro")));
-				registro.setTipo(tipo);
-				registro.setCategoria(res.getString("ds_categoria"));
-				registro.setDescricao(res.getString("ds_registro"));
-
+				registro = this.handleQuery(res);
 			} 
 
 		} catch (SQLException e) {
@@ -185,8 +183,8 @@ public class RegistroDAO {
 			stmt.setString(4, registro.getDescricao());
 			stmt.setInt(5, registro.getCodigo());
 			
-			System.out.println("Registro atualizado");
 			stmt.executeUpdate();
+			System.out.println("Registro atualizado");
 
 		} catch (SQLException e) {
 			e.printStackTrace();
@@ -208,12 +206,15 @@ public class RegistroDAO {
 		PreparedStatement stmt = null;
 		try {
 			connection = OracleDBConnection.getInstance().getConnection();
-			String sql = "DELETE FROM T_FINTECH_REGISTRO WHERE cd_registro = " + codigoRegistro 
-					+ " AND cd_usuario = " + codigoUsuario;
+			String sql = "DELETE FROM T_FINTECH_REGISTRO WHERE cd_registro = ?"  
+					+ " AND cd_usuario = ?";
 			stmt = connection.prepareStatement(sql);
 			
-			System.out.println("Registro deletado");
+			stmt.setInt(1, codigoRegistro);
+			stmt.setInt(2, codigoUsuario);
+			
 			stmt.executeUpdate();
+			System.out.println("Registro deletado");
 
 		} catch (SQLException e) {
 			e.printStackTrace();
